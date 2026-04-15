53e21902ae23f5a7ef7df1ea5cfd0b1deebe1651
PC40 <pc@40.com>
2026-04-15 07:20:33 +0700

    SEO ganteng

--- day1/web-berita/detail.php
+++ day1/web-berita/detail.php
@@ -6,6 +6,6 @@ $id = isset($_GET["id"]) ? (int) $_GET["id"] : 0;
 $data=mysqli_fetch_assoc(mysqli_query($con,"select * from berita where id=$id"));
 ?>
 
-<h2 <?= $data['judul'] ?>></h2>
-<p<?= $data['isi'] ?>></p>
+<h2 ><?= $data["judul"] ?></h2>
+<p><?= $data["isi"] ?></p>
 <a href="index.php">kembali</a>
