24d43b75450d4347796b3fff5c7066a1fcbf9daf
PC40 <apasihsh@gmail.com>
2026-04-17 10:21:49 +0700

    a

--- day4/berita/resources/views/berita/index.blade.php
+++ day4/berita/resources/views/berita/index.blade.php
@@ -1,17 +1,19 @@
 
 <main class="anitaliased">
     <x-navbar></x-navbar>
-    <div>
+    <div class="flex text-4xl justify-between mt-10 bg-amber-100 pt-2 pb-2 pl-10 pr-10 shadow-2xl border-l">
         <h2 class="">data berita</h2>
         <a href="/berita/create">tambah</a>
     </div>
     <div class="max-w-7xl mx-auto p-6 lg:p-8">
          @foreach ($data as $b )
-            <h3 class="mt-6 text-xl font-semibold text-gray-900">{{ $b->judul }}</h3>
-            <p class="mt-4 text-gray-800 text-sm leading-relaxed">{{ $b->isi }}</p>
-            <div class="">
-                <a class="" href="/berita/delete/{{ $b->id }}">hapus</a>
-            </div>
+         <div class="border-l pl-2 text-5xl">
+             <h3 class="mt-6 font-semibold text-gray-900">{{ $b->judul }}</h3>
+             <p class="mt-4 text-xl text-gray-800 leading-relaxed">{{ $b->isi }}</p>
+             <div class="mt-10 text-red-500">
+                 <a class="bg-gray-100 " href="/berita/delete/{{ $b->id }}">hapus</a>
+             </div>
+         </div>
         @endforeach
     </div>
 </main>
